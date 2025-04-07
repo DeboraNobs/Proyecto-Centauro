1a7ee74e91f073a5706bc40c6d6ecd054a2cd98f
Debora Vanesa Nobs <dnobs@centauro.net>
2025-04-07 11:31:12 +0200

    Cambios en obj y bin

--- obj/Debug/net8.0/proyecto_centauro.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyecto_centauro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_centauro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae2186fb6ebe46803239b9746ab4644382c812f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738af1ef585e559b4568f45b92e287b58f7eaf58")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_centauro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_centauro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
